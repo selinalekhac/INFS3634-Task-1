86f5e0fe049b0e08056395717d2909a3ce6849ab
Selina Le Khac <selinalekhac@Selinas-MacBook-Pro.local>
2019-09-29 20:45:26 +1000

    chunk added

--- app/src/main/java/com/example/infs3634hwtask1/MainActivity.java
+++ app/src/main/java/com/example/infs3634hwtask1/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /* hello */
-    System.out.println("Hello I'm Selina!");
+
 
 }

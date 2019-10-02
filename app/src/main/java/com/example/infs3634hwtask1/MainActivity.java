a6762eec07473a114d37b46dccf5c99a59b75d3a
Selina Le Khac <selinalekhac@Selinas-MacBook-Pro.local>
2019-10-02 18:24:11 +1000

    added in a digital clock however the time isnt set yet

--- app/src/main/java/com/example/infs3634hwtask1/MainActivity.java
+++ app/src/main/java/com/example/infs3634hwtask1/MainActivity.java
@@ -61,78 +61,13 @@ public class MainActivity extends AppCompatActivity {
         layoutCityNameText.setText(R.string.WashingtonDC);
         layoutCityImage = layoutWashingtonDC.findViewById(R.id.city_imageview_id);
         layoutCityImage.setImageResource(R.drawable.washingtondc);
-/*
-        ConstraintLayout layoutSydney = findViewById(R.id.layout_sydney);
 
-        TextView layoutSydneyText = layoutSydney.findViewById(R.id.city_textview_id);
-        layoutSydneyText.setText(R.string.Sydney);
 
-        ImageView layoutSydneyImage = layoutSydney.findViewById(R.id.city_imageview_id);
-        layoutSydneyImage.setImageResource(R.drawable.sydneyharbourbridge);
 
- */
-
-/*
-        ConstraintLayout layoutSydney = findViewById(R.id.layout_Sydney);
-        layoutSydneyText = layoutSydney.findViewById(R.id.cityTextView);
-        layoutSydneyText.setText(R.string.sydney);
-        layoutSydneyImage = layoutSydney.findViewById(R.id.cityImageView);
-        layoutSydneyImage.setImageResource(R.drawable.sydneyharbourbridge);
-        */
     }
 
 
 
-/*
-From the Lecture Slides
 
-    ConstraintLayout buttonTrain = findViewById(R.id.buttonTrain);
-    TextView buttonTrainText = buttonTrain.findViewById(R.id.tv__text);
-    buttonTrainText.setText(R.string.train);
-    ImageView buttonTrainImage = buttonTrain.findViewById(R.id.iv_pic);
-    buttonTrainImage.setImageResource(R.drawable.train):
-*/
-
- /*
-   Time hasn't been attempted yet
-   --> What do you put in the findBtViewId () Lerma0
-
-    FOR SYDNEY
-    ConstraintLayout buttonSydney = findViewById(R.id.buttonSydney);
-    TextView buttonSydneyText = buttonSydney.findViewById(R.id.);
-    buttonSydneyText.setText(R.string.Sydney)
-    ImageView buttonSydneyImage = buttonSydney.findViewById(R.id.);
-    buttonSydneyImage.setImageResource(R.drawable.sydneyharbourbridge);
-
-    FOR WASHINGTON DC
-    ConstraintLayout buttonWashingtonDC = findViewById(R.id.buttonWashingtonDC);
-    TextView buttonWashingtonDCText = buttonWashingtonDC.findViewById(R.id.);
-    buttonWashingtonDCText.setText(R.string.WashingtonDC)
-    ImageView buttonWashingtonDCImage = buttonWashingtonDC.findViewById(R.id.);
-    buttonWashingtonDCImage.setImageResource(R.drawable.washingtondc);
-
-    FOR SINGAPORE
-    ConstraintLayout buttonSingapore = findViewById(R.id.buttonSingapore);
-    TextView buttonSingaporeText = buttonSingapore.findViewById(R.id.);
-    buttonSingaporeText.setText(R.string.Singapore)
-    ImageView buttonSingaporeImage = buttonSingapore.findViewById(R.id.);
-    buttonSingaporeImage.setImageResource(R.drawable.singapore);
-
-    FOR VIETNAM
-    ConstraintLayout buttonVietnam = findViewById(R.id.buttonVietnam);
-    TextView buttonVietnamText = buttonVietnam.findViewById(R.id.);
-    buttonVietnamText.setText(R.string.Vietnam)
-    ImageView buttonVietnamImage = buttonVietnam.findViewById(R.id.);
-    buttonVietnamImage.setImageResource(R.drawable.vietnamehalongbay);
-
-    FOR RIO DE JANEIRO
-    ConstraintLayout buttonRioDeJaneiro = findViewById(R.id.buttonRioDeJaneiro);
-    TextView buttonRioDeJaneiroText = buttonRioDeJaneiro.findViewById(R.id.);
-    buttonRioDeJaneiroText.setText(R.string.RioDeJaneiro)
-    ImageView buttonRioDeJaneiroImage = buttonRioDeJaneiro.findViewById(R.id.);
-    buttonRioDeJaneiroImage.setImageResource(R.drawable.riodejaneiro);
-
-
-`*/
 
 }
